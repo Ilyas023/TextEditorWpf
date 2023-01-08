e49e9674b5dc34a012e5ab3965abe4f8bca582f0
Ilyas <ilias_03@bk.ru>
2023-01-09 03:29:10 +0600

    Add translate app

--- TextRedaction/MainWindow.xaml.cs
+++ TextRedaction/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
@@ -15,12 +16,28 @@ namespace TextRedaction
     /// </summary>
     public partial class MainWindow : Window
     {
+        RichTextBox RichTextList = new RichTextBox();
+        string Now = "";
+
+        
         public MainWindow()
         {
             InitializeComponent();
 
             FamilyChoice.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
             FamilyChoiceMenu.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+
+            RichTextList.Name = TextEl.Name;
+        }
+
+        private void ItemNew_Click(object sender, RoutedEventArgs e)
+        {
+            Now = "New";
+            Products.Items.Add(new TabItem
+            {
+                Header = new TextBlock { Text = Now }, 
+                Content = RichTextList
+            });
         }
 
         private void btnLeft_Click(object sender, RoutedEventArgs e)
@@ -147,5 +164,66 @@ namespace TextRedaction
         {
             btnLeft_Click(sender, e);
         }
+
+        private void LanguageEng_Click(object sender, RoutedEventArgs e)
+        {
+            ItemFile.Header = "File";
+            ItemNew.Header = "New";
+            ItemOpen.Header = "Open";
+            ItemSave.Header = "Save";
+            exitMenu.Header = "Exit";
+            ItemEdit.Header = "Edit";
+            ItemCut.Header = "Cut";
+            ItemCopy.Header = "Copy";
+            ItemPaste.Header = "Paste";
+            ItemFormat.Header = "Format";
+            ItemAlignment.Header = "Alignment";
+            ItemLeft.Header = "Left";
+            ItemCenter.Header = "Center";
+            ItemRight.Header = "Right";
+            ItemJustify.Header = "Justify";
+            TextColor.Text = "Color: ";
+            TextBlack.Text = "Black";
+            TextBlue.Text = "Blue";
+            TextGreen.Text = "Green";
+            TextOrange.Text = "Orange";
+            TextPurple.Text = "Purple";
+            TextRed.Text = "Red";
+            TextYellow.Text = "Yellow";
+            TextCol.Text = "Col: ";
+            TextRow.Text = "Row: ";
+            fontMenu.Header = "Font";
+        }
+
+        private void LanguageRu_Click(object sender, RoutedEventArgs e)
+        {
+            ItemFile.Header = "Файл";
+            ItemNew.Header = "Новый файл";
+            ItemOpen.Header = "Открыть";
+            ItemSave.Header = "Сохранить";
+            exitMenu.Header = "Закрыть";
+            ItemEdit.Header = "Изменить";
+            ItemCut.Header = "Вырезать";
+            ItemCopy.Header = "Скопировать";
+            ItemPaste.Header = "Вставить";
+            ItemFormat.Header = "Формат";
+            ItemAlignment.Header = "Расположение";
+            ItemLeft.Header = "Налево";
+            ItemCenter.Header = "Центр";
+            ItemRight.Header = "Направо";
+            ItemJustify.Header = "Выровнить";
+            TextColor.Text = "Цвет: ";
+            TextBlack.Text = "Черный";
+            TextBlue.Text = "Синий";
+            TextGreen.Text = "Зеленый";
+            TextOrange.Text = "Оранжевый";
+            TextPurple.Text = "Фиолетовый";
+            TextRed.Text = "Красный";
+            TextYellow.Text = "Желтый";
+            TextCol.Text = "Столбец: ";
+            TextRow.Text = "Строка: ";
+            fontMenu.Header = "Шрифт";
+        }
+
     }
 }
